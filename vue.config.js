fd0830532120ff0a3601dc0a0a9859f17d642054
sapazutku <64533036+sapazutku@users.noreply.github.com>
2021-09-10 19:35:58 +0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                data: `@import "@/styles/_variables.scss";`
-            }
-        }
-    }
-};
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/my-project/'
+        : '/'
+}
